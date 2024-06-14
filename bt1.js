d612f46ad5d44a95e0fa394f7c78ef5ae49eda67
Admin <lmht721@gmail.com>
2024-06-14 17:04:53 +0700

    add

---
+++ bt1.js
@@ -0,0 +1,10 @@
+let number;
+let tatol = 0;
+// let tatol = 0;
+while (number !== -1) {
+  number = +prompt("Nhap so:");
+  tatol += number;
+  console.log(tatol);
+}
+alert("So vua nhap la -1");
+
